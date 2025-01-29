3829cf8e642d24e91dd1b99af4f916cfda94779e
Kazutaka Ikemura <banbara.work@gmail.com>
2025-01-29 18:09:38 +0900

    feat: Spotless導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,26 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.spotless)
+}
+
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    kotlin {
+        ktlint("1.5.0")
+            .setEditorConfigPath("$projectDir/.editorconfig")
+            .editorConfigOverride(
+                mapOf(
+                    "package-name" to "disabled",
+                ),
+            )
+        target("**/*.kt")
+        targetExclude("**/build/**")
+        // trimTrailingWhitespace()
+        // endWithNewline()
+    }
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktlint("1.5.0")
+            .setEditorConfigPath("$projectDir/.editorconfig")
+    }
 }
