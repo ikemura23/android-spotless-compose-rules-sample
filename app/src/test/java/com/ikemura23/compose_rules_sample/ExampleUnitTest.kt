d58ea4e7f523a4bb22e1a721c3261794863348cd
Kazutaka Ikemura <banbara.work@gmail.com>
2025-01-29 18:14:43 +0900

    refactor: spotlessApplyタスクを実行した

--- app/src/test/java/com/ikemura23/compose_rules_sample/ExampleUnitTest.kt
+++ app/src/test/java/com/ikemura23/compose_rules_sample/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.ikemura23.compose_rules_sample
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "android-compose-rules-sample"
 include(":app")
- 
